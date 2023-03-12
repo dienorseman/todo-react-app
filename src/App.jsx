64aa83fa27cf86ced84e286369e43c9686c2000b
redgrin <edgar.elps@gmail.com>
2023-03-12 03:28:13 -0500

    edit button and delete button functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
 import { useState } from "react";
 
 function App() {
-  const [ newTodo, setNewTodo ] = useState('')
-  const [todos, setTodos] = useState([
-    "read clean code",
-    "write a book about vampires",
-    "go to the park",
-  ]);
+
+  const [newTodo, setNewTodo] = useState({});
+  const [nextId, setNextId] = useState(1);
+  const [todos, setTodos] = useState([]);
 
   const onHandleSubmit = (event) => {
-    event.preventDefault()
-    setTodos(
-      [ newTodo, ...todos ]
-    )
+    event.preventDefault();
+    setTodos([ ...todos, newTodo ]);
+    setNewTodo({});
+    setNextId(nextId + 1);
+
+  };
+
+  const handleDeleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+    // console.log(id);
+  };
+
+  const handleEditTodo = (id) => {
+    return
   }
 
   return (
     <>
       <main>
-        <form className="todo-form" onSubmit={ onHandleSubmit }>
-          <input 
-          type="text" 
-          placeholder="What do we need to do?" 
-          onChange={( event )=> setNewTodo( event.target.value )}
+        <form className="todo-form" onSubmit={onHandleSubmit}>
+          <input
+            type="text"
+            placeholder="What do we need to do?"
+            value={newTodo.content || ""}
+            onChange={(event) => {
+              setNewTodo({
+                id: nextId,
+                content: event.target.value,
+              });
+              // console.log(newTodo);
+            }}
           />
           <button>Add Todo</button>
         </form>
         <div className="todos-container">
           <ul>
-            {todos.map(( item , index ) => (
-              <li key={ index }> <p>{ item }</p><button>X</button></li>
+            {todos.map((todo) => (
+              <li key={todo.id}>
+                <p>
+                  {todo.content}
+                </p>
+                <button  onClick={ handleEditTodo } >edit</button>
+                <button onClick={ () => handleDeleteTodo(todo.id) } >X</button>
+              </li>
             ))}
           </ul>
         </div>
