e137f7a03352a8d38a45b780dba20aa8b78ad07b
redgrin <norz34z0@gmail.com>
2023-03-15 16:45:19 -0600

    bootstrap added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,50 @@
-import { useReducer } from "react"
-import { todoReducer } from "./hooks/todoReducer"
+import { useReducer } from "react";
+import { todoReducer } from "./hooks/todoReducer";
 
 export const App = () => {
-
   const initialState = [
     {
       id: 1,
-      description: 'Jump in circles',
+      description: "Jump in circles",
       done: false,
     },
     {
-      id: 2, 
-      description: 'go to the park',
+      id: 2,
+      description: "go to the park",
       done: false,
     },
     {
       id: 3,
-      description: 'Grab a beer',
+      description: "Grab a beer",
       done: false,
-    }
-  ]
+    },
+  ];
 
-  const [state, dispatch] = useReducer( todoReducer, initialState)
+  const [state, dispatch] = useReducer(todoReducer, initialState);
 
   return (
     <div>
       <h1>Todo App</h1>
-      <ul>
-          <li>Item 1</li>
-      </ul>
+      <div className="row">
+        <div className="col-7">
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+          </ul>
+        </div>{" "}
+        <div className="col-5">
+          <h4>Add Todo</h4>
+          <hr />
+          <form>
+            <input
+              type="text"
+              className="What needs to be done?"
+              placeholder="¿Qué hay que hacer?"
+            />
+          </form>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
